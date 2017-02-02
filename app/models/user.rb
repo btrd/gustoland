276aaacd5ab0e80a7205009ad933a4f2ba23e2af
Jean Bertrand <jeanbertrand.contact@gmail.com>
2017-02-02 17:00:42 +0100

    Validates user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :nickname, :email, :name, :password, presence: true
+  validates :email, :nickname, uniqueness: true
+  validates :password, length: { minimum: 8 }
 end
