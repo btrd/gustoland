18579806ccad87ccd7bee98bd6152852edb47d1c
Jean Bertrand <jeanbertrand.contact@gmail.com>
2017-03-02 21:08:53 +0100

    Fix recipe controller, doesnt need auth

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,34 +1,28 @@
 module Api
   module V1
     class RecipesController < ApplicationController
-      before_action :authenticate_user, only: [:create, :update, :destroy]
-
       before_action :set_recipe, only: [:show, :update, :destroy]
 
-      # GET /recipes
       def index
         @recipes = Recipe.all
 
         render json: @recipes
       end
 
-      # GET /recipes/1
       def show
         render json: @recipe
       end
 
-      # POST /recipes
       def create
         @recipe = Recipe.new(recipe_params)
 
         if @recipe.save
-          render json: @recipe, status: :created, location: @recipe
+          render json: @recipe, status: :created
         else
           render json: @recipe.errors, status: :unprocessable_entity
         end
       end
 
-      # PATCH/PUT /recipes/1
       def update
         if @recipe.update(recipe_params)
           render json: @recipe
@@ -37,19 +31,18 @@ module Api
         end
       end
 
-      # DELETE /recipes/1
       def destroy
         @recipe.destroy
       end
 
       private
-        def set_recipe
-          @recipe = Recipe.find(params[:id])
-        end
+      def set_recipe
+        @recipe = Recipe.find(params[:id])
+      end
 
-        def recipe_params
-          params.require(:recipe).permit(:description, :price, :time, :serving, :publication)
-        end
+      def recipe_params
+        params.permit(:description, :price, :time, :serving, :publication)
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      post 'user_token' => 'user_token#create'
+      post 'user_token', to: 'user_token#create'
       resources :users, only: [:show, :create]
       put 'users', to: 'users#update'
 
