819beffbf1354d0c8e64add366ba9dde2011617c
Jean Bertrand <jeanbertrand.contact@gmail.com>
2017-03-03 22:53:40 +0100

    Include tags in Recipe endpoint

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -5,15 +5,19 @@ module Api
 
       def index
         @recipes = Recipe.all
-        render json: @recipes
+        render json: @recipes.as_json(include: { tag: {only: :name} })
       end
 
       def show
-        render json: @recipe
+        render json: @recipe.as_json(include: { tag: {only: :name} })
       end
 
       def create
         @recipe = Recipe.new(recipe_params)
+
+        tags_params.each do |tag|
+          @recipe.tag << Tag.find_or_create_by(name: tag)
+        end
 
         if @recipe.save
           render json: @recipe, status: :created
@@ -41,6 +45,10 @@ module Api
 
       def recipe_params
         params.permit(:description, :price, :time, :serving, :publication, :likes, steps: [])
+      end
+
+      def tags_params
+        params.permit(:tags)
       end
     end
   end
