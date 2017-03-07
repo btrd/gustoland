fc5c5fe5f9988c9ad5548d98e635a207e827bd20
Jean Bertrand <jeanbertrand.contact@gmail.com>
2017-03-07 21:10:17 +0100

    Add informations in User endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,11 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      before_action :authenticate_user, except: [:create]
+      before_action :authenticate_user, only: [:update]
 
       def show
-        render json: current_user
+        user = User.find(params[:id])
+        render json: user.as_json(include: [:recipes, :follow, :follow_by])
       end
 
       def create
