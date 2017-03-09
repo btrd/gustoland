228990b367817050b8ff0589fc21aa51cbd365fb
Jean Bertrand <jeanbertrand.contact@gmail.com>
2017-03-09 16:24:30 +0100

    Tag are always lovercase

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,12 @@
 class Tag < ApplicationRecord
   has_and_belongs_to_many :recipes
 
+  before_save :downcase_name
+
+  def downcase_name
+    self.name.downcase!
+  end
+
   def image
     recipes.sort_by { |r| r.likes + r.books }.first.image
   end
