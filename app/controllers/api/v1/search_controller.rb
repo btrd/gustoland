f26ce8473c5932941ff79bb7737ad3d637d84973
Jean Bertrand <jeanbertrand.contact@gmail.com>
2017-03-10 10:20:16 +0100

    Try another sort for recipes

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -18,7 +18,7 @@ module Api
         if params[:sort] || params[:sort] == 'date'
           res_recipes.sort_by! { |r| r.created_at }.reverse!
         else
-          res_recipes.sort_by! { |r| -(r.likes + r.books) }
+          res_recipes.sort_by! { |r| r.likes + r.books }
         end
 
         render json: res_recipes.uniq.as_json(
